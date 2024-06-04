3059a9ccd27c9775db45f3b893ed021af88b2db6
mikhraly <mikhraly@gmail.com>
2024-06-04 22:34:03 +0300

    add HW11 folder

---
+++ HW11/temp_api.c
@@ -0,0 +1,29 @@
+#include "temp_api.h"
+
+int main()
+{
+    temperature_sensor temp_sensor[1000];
+
+    return 0;
+}
+
+
+void print_average_temp(int year, int month)
+{
+
+}
+
+void print_minimum_temp(int year, int month)
+{
+
+}
+
+void print_maximum_temp(int year, int month)
+{
+
+}
+
+void print_statistic(int year)
+{
+    
+}
